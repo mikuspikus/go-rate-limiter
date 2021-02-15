b91ec8aef9a5de95fe9a9b1d60239232a732afc1
mikuspikus <pyro74rus@gmial.com>
2021-02-15 12:39:07 +0300

    Fix comments

--- storage.go
+++ storage.go
@@ -10,7 +10,7 @@ import (
 var ErrStopped = fmt.Errorf("store is stopped")
 
 type Storage interface {
-	// Take takes the token from a memstorage by a given key if available and returning:
+	// Take takes the token from a storage by a given key if available and returning:
 	// 	- limit size
 	//	- number of remaining tokens for the interval
 	// 	- server time when token will be available
@@ -19,7 +19,7 @@ type Storage interface {
 	// If "ok" was false you should not serve request further
 	Take(ctx context.Context, key string) (tokens, remaining, reset uint64, ok bool, err error)
 	// Get returns current limit and remaining tokens for given key.
-	// Does not change state of the memstorage
+	// Does not change state of the storage
 	Get(ctx context.Context, key string) (tokens, remaining uint64, err error)
 	// Set setups the limit and interval for a provided key.
 	// If key already exists then it would be overwrittten
@@ -27,7 +27,7 @@ type Storage interface {
 	// Burst add more tokens to the the key's current bucket until next interval tick.
 	// This may lead current bucket interval tick to exceed the maximum number of ticks until next interval.
 	Burst(ctx context.Context, key string, tokens uint64) error
-	// Close terminates the memstorage and cleans up any data structures or connections.
+	// Close terminates the storage and cleans up any data structures or connections.
 	// After Close(), Take() should always return zero
 	Close(ctx context.Context) error
 }
